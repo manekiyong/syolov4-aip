b370b100d7395832e8b95c3d33d42507b92de5ff
manekiyong <59855081+manekiyong@users.noreply.github.com>
2022-06-18 13:46:51 +0800

    use diff img, update req

--- src/main.py
+++ src/main.py
@@ -38,18 +38,18 @@ if __name__ == "__main__":
     Task.force_requirements_env_freeze(force=True, requirements_file='../requirements.txt')
     clearml_task = Task.init(project_name=PROJECT_NAME, task_name='syolov4_'+train_args['name'])
     if args.remote:
-        clearml_task.set_base_docker("nvidia/cuda:11.4.0-runtime-ubuntu20.04",
+        clearml_task.set_base_docker("nvidia/cuda:11.4.0-devel-ubuntu20.04",
             docker_setup_bash_script=['pip3 show wheel', 
                                     'pip show wheel', 
                                     'apt-get update && apt-get install libgl1 -y']
         )
         clearml_task.execute_remotely(queue_name="compute")
 
-        cwd = os.getcwd()
-        os.chdir('mish-cuda/dist')
-        subprocess.run(['pip3', 'install', 'mish_cuda-0.0.3-cp38-cp38-linux_x86_64.whl'])
-        os.chdir(cwd)
-        os.environ['CUDA_LAUNCH_BLOCKING'] = '1' 
+        # cwd = os.getcwd()
+        # os.chdir('mish-cuda/dist')
+        # subprocess.run(['pip3', 'install', 'mish_cuda-0.0.3-cp38-cp38-linux_x86_64.whl'])
+        # os.chdir(cwd)
+        # os.environ['CUDA_LAUNCH_BLOCKING'] = '1' 
 
     # only putting the import statement here because i need to install 
     # the damn mish before i could move on with life, otherwise in normal 
